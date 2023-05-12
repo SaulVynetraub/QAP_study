61a526ad368e7f457b8d56216bff07babe9913a9
Daniil <ld1357@yandex.ru>
2023-05-12 15:11:33 +0300

    Cleaning and update repository

---
+++ pythonProjectTest/cat.py
@@ -0,0 +1,9 @@
+from class_21 import Cat
+
+cat_1 = Cat("Baron", "Boy", 2)
+print(f"The characteristics of the first cat: {cat_1.name}, {cat_1.gender}, {cat_1.age}.")
+print("He can", cat_1.cat_meow())
+
+cat_2 = Cat("Sam", "Boy", 2)
+print(f"The characteristics of the second cat: {cat_2.name}, {cat_2.gender}, {cat_2.age}.")
+print("He can", cat_2.cat_meow())
